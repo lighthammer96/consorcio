d9d3e14840fe3c072e3132ea8914e2626d11b315
lighthammer96 <jhoan.fury@gmail.com>
2022-11-28 18:26:36 -0500

    reload reporte documentos emitidos

--- app/Http/Recopro/ReporteDocumentosEmitidos/ReporteDocumentosEmitidosRepository.php
+++ app/Http/Recopro/ReporteDocumentosEmitidos/ReporteDocumentosEmitidosRepository.php
@@ -24,8 +24,10 @@ class ReporteDocumentosEmitidosRepository implements ReporteDocumentosEmitidosIn
         $model = $this->model;
 
         if(!empty($_REQUEST["FechaInicioFiltro"]) && !empty($_REQUEST["FechaFinFiltro"])) {
-            $inicio = str_replace("-", "/", $_REQUEST["FechaInicioFiltro"]);
-            $fin = str_replace("-", "/", $_REQUEST["FechaFinFiltro"]);
+            // $inicio = str_replace("-", "/", $_REQUEST["FechaInicioFiltro"]);
+            // $fin = str_replace("-", "/", $_REQUEST["FechaFinFiltro"]);
+            $inicio = $_REQUEST["FechaInicioFiltro"];
+            $fin = $_REQUEST["FechaFinFiltro"];
             $model = $model->whereBetween('FechaEmisionServer', [$inicio, $fin]);
         }
 
